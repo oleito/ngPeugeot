feabfb398f58dbbf239925889e393efcc34e6165
Leandro <oleito@gmail.com>
2019-12-19 01:27:29 -0300

    inicia reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngPeugeot';
+
+  contacto1 = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    telefono: new FormControl('', Validators.required),
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    area: new FormControl('', Validators.required),
+    ciudad: new FormControl('', Validators.required),
+    consulta: new FormControl('', Validators.required)
+  });
+  contacto2 = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    consulta: new FormControl('', Validators.required)
+  });
+  onSubmit1(){
+    console.log(this.contacto1.value);
+  } 
+  onSubmit2(){
+    console.log(this.contacto2.value);
+  }
 }
