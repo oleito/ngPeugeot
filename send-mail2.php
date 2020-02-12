099d81bc2e1bd34c66852702a4bd28796faf4b93
Leandro <oleito@gmail.com>
2020-02-12 11:45:54 -0300

    Actualizacion febrero

--- send-mail2.php
+++ send-mail2.php
@@ -5,10 +5,10 @@
 
 	$para = 'sistemas@parisautos.com.ar';
 
-	$titulo = 'Mensaje desde la web - ParisAutos';
+	$titulo = 'Mensaje desde la web - ParisPeugeot.com.ar';
 	$mensaje = '<html>';
 	$mensaje = '<body>';
-	$mensaje = 'Mensaje enviado desde sitio WEB:';
+	$mensaje = 'Mensaje desde formulario WEB:';
 	$mensaje .= '<br> Nombre: ' . $name . "\r\n";
 	$mensaje .= '<br> Telefono: ' . $tel . "\r\n";
 	$mensaje .= '<br> Mensaje: ' . $message . "\r\n";
@@ -24,8 +24,8 @@ mail($para, $titulo, $mensaje, $cabeceras);
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<script>alert("Su solicitud de turno ha sido enviada. Nos pondremos en contacto con usted lo mas pronto posible.");</script>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<script>alert("Su consulta ha sido enviada. Nos pondremos en contacto con usted lo mas pronto posible.");</script>
 	<meta HTTP-EQUIV="REFRESH" content="0; url=index.html">
 
 </head>
