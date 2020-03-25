f58dc3a9901eda7ce0c1cb41454e601371bff54c
Leandro <oleito@gmail.com>
2020-03-25 18:43:00 -0300

    Avanza migracion a vue

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: "#appModelos",
+  data: {
+    message: "Hello Vue!",
+    modelos: [
+      { img: "Learn JavaScript", nombre: "208", manual: "stc" },
+      { text: "Learn Vue" },
+      { text: "Build something awesome" }
+    ]
+  }
+});
