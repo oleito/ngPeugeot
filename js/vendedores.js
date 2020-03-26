827dbf20c72d86d3a5e41474f1e37e14c7009798
Leandro <oleito@gmail.com>
2020-03-26 20:18:07 -0300

    Pre listo

--- js/vendedores.js
+++ js/vendedores.js
@@ -1,14 +1,56 @@
 var appVendedores = new Vue({
-    el: '#appVendedores',
-    data: {
-        vendedores: [
-            { img: 'Pfernandez.jpg', nombre:'Pamela Fernandez', sector:"Recepcion", cel:"2664123456", email:"recepcion@parisautos.com.ar"},
-            { img: 'Slucero.jpg', nombre:'Soledad Lucero', sector:"Ejecutivo de ventas", cel:"2664123456", email:"soledad.lucero@parisautos.com.ar"},
-            { img: 'Jalmazan.jpg', nombre:'Javier Almazan', sector:"Ejecutivo de ventas", cel:"2664123456", email:"javier.almazan@parisautos.com.ar"},
-            { img: 'Lpetrovithc.jpg', nombre:'Luiz Petrovithc', sector:"Ejecutivo de ventas", cel:"2664123456", email:"luiz.petrovithc@parisautos.com.ar"},
-            { img: 'Cgonzalez.jpg', nombre:'Carolina Gonzales', sector:"Ejecutivo de ventas", cel:"2664123456", email:"carolina.gonzales@parisautos.com.ar"},
-            { img: 'Gdominguez.jpg', nombre:'Guillermo Dominguez', sector:"Ejecutivo de ventas", cel:"2664123456", email:"guillermo.dominguez@parisautos.com.ar"},
-            { img: 'Mmartin.jpg', nombre:'Mauricio Martin', sector:"Ejecutivo de ventas", cel:"2664123456", email:"mauricio.martin@parisautos.com.ar"},
-        ]
-    }
-  })
+  el: "#appVendedores",
+  data: {
+    vendedores: [
+      {
+        img: "Jalmazan.jpg",
+        nombre: "Javier Almazan",
+        sector: "Ejecutivo de ventas",
+        cel: "2664572936",
+        email: "javier.almazan@parisautos.com.ar"
+      },
+      {
+        img: "Gdominguez.jpg",
+        nombre: "Guillermo Dominguez",
+        sector: "Ejecutivo de ventas",
+        cel: "2665103442",
+        email: "guillermo.dominguez@parisautos.com.ar"
+      },
+      {
+        img: "Cgonzalez.jpg",
+        nombre: "Carolina Gonzales",
+        sector: "Ejecutivo de ventas",
+        cel: "2664346591",
+        email: "carolina.gonzales@parisautos.com.ar"
+      },
+      {
+        img: "Slucero.jpg",
+        nombre: "Soledad Lucero",
+        sector: "Ejecutivo de ventas",
+        cel: "2664883826",
+        email: "soledad.lucero@parisautos.com.ar"
+      },
+      {
+        img: "Mmartin.jpg",
+        nombre: "Mauricio Martin",
+        sector: "Ejecutivo de ventas",
+        cel: "2664653757",
+        email: "mauricio.martin@parisautos.com.ar"
+      },
+      {
+        img: "Lpetrovithc.jpg",
+        nombre: "Luiz Petrovithc",
+        sector: "Ejecutivo de ventas",
+        cel: "2665133494",
+        email: "luiz.petrovithc@parisautos.com.ar"
+      },
+      {
+        img: "Pfernandez.jpg",
+        nombre: "Pamela Fernandez",
+        sector: "Recepcion",
+        cel: "2664303957",
+        email: "recepcion@parisautos.com.ar"
+      }
+    ]
+  }
+});
