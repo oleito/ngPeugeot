ce1b4c92e607f64ae9d5c9d901b0d004e8528b64
Leandro <oleito@gmail.com>
2020-04-07 03:04:19 -0300

    primera plantilla de planes

---
+++ js/vendedores-planes.js
@@ -0,0 +1,70 @@
+var appVendedores = new Vue({
+  el: "#appVendedores",
+  data: {
+    vendedores: [
+      {
+        img: "Dmoreno.jpg",
+        nombre: "Javier Almazan",
+        sector: "Ejecutivo de ventas",
+        cel: "2664572936",
+        email: "javier.almazan@parisautos.com.ar",
+      },
+      {
+        img: "Adavid.jpg",
+        nombre: "Guillermo Dominguez",
+        sector: "Ejecutivo de ventas",
+        cel: "2665103442",
+        email: "guillermo.dominguez@parisautos.com.ar",
+      },
+      {
+        img: "Aballerino.jpg",
+        nombre: "Carolina Gonzalez",
+        sector: "Ejecutivo de ventas",
+        cel: "2664346591",
+        email: "carolina.gonzalez@parisautos.com.ar",
+      },
+      {
+        img: "Adessi.jpg",
+        nombre: "Soledad Lucero",
+        sector: "Ejecutivo de ventas",
+        cel: "2664883826",
+        email: "soledad.lucero@parisautos.com.ar",
+      },
+      {
+        img: "Frodriguez.jpg",
+        nombre: "Soledad Lucero",
+        sector: "Ejecutivo de ventas",
+        cel: "2664883826",
+        email: "soledad.lucero@parisautos.com.ar",
+      },
+      {
+        img: "Fsutil.jpg",
+        nombre: "Soledad Lucero",
+        sector: "Ejecutivo de ventas",
+        cel: "2664883826",
+        email: "soledad.lucero@parisautos.com.ar",
+      },
+      {
+        img: "Jortiz.jpg",
+        nombre: "Soledad Lucero",
+        sector: "Ejecutivo de ventas",
+        cel: "2664883826",
+        email: "soledad.lucero@parisautos.com.ar",
+      },
+      {
+        img: "Lcorzo.jpg",
+        nombre: "Soledad Lucero",
+        sector: "Ejecutivo de ventas",
+        cel: "2664883826",
+        email: "soledad.lucero@parisautos.com.ar",
+      },
+      {
+        img: "Rmercado.jpg",
+        nombre: "Soledad Lucero",
+        sector: "Ejecutivo de ventas",
+        cel: "2664883826",
+        email: "soledad.lucero@parisautos.com.ar",
+      }
+    ],
+  },
+});
