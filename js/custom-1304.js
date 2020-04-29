d4ea03c279bdb566265b5e9b851d51d69ddc29d2
Leandro <oleito@gmail.com>
2020-04-29 13:58:29 -0300

    PLanes

--- js/custom-1304.js
+++ js/custom-1304.js
@@ -7,66 +7,66 @@ var app = new Vue({
         img: "208.jpg",
         nombre: "208",
         manual: "208.pdf",
-        ficha: "assets/fichas/FICHA TECNICA_208.pdf"
+        ficha: "FICHA TECNICA_208.pdf"
       },
       {
         img: "308.jpg",
         nombre: "308",
         manual: "308.pdf",
-        ficha: "assets/fichas/FICHA TECNICA_308.pdf"
+        ficha: "FICHA TECNICA_308.pdf"
       },
       {
         img: "408.jpg",
         nombre: "408",
         manual: "408.pdf",
-        ficha: "assets/fichas/FICHA TECNICA_408.pdf"
+        ficha: "FICHA TECNICA_408.pdf"
       },
       {
         img: "2008.jpg",
         nombre: "2008",
         manual: "2008.pdf",
-        ficha: "assets/fichas/FICHA TECNICA_2008.pdf"
+        ficha: "FICHA TECNICA_2008.pdf"
       },
       {
         img: "3008.jpg",
         nombre: "3008",
         manual: "3008.pdf",
-        ficha: "assets/fichas/FICHA TECNICA_3008.pdf"
+        ficha: "FICHA TECNICA_3008.pdf"
       },
       {
         img: "5008.jpg",
         nombre: "5008",
         manual: "5008.pdf",
-        ficha: "assets/fichas/FICHA TECNICA_5008.pdf"
+        ficha: "FICHA TECNICA_5008.pdf"
       },
       {
         img: "boxer.jpg",
         nombre: "Boxer",
         manual: "boxer.pdf",
-        ficha: "assets/fichas/FICHA TECNICA_BOXER.pdf"
+        ficha: "FICHA TECNICA_BOXER.pdf"
       },
       {
         img: "expert.jpg",
         nombre: "Expert",
         manual: "expert.pdf",
-        ficha: "assets/fichas/FICHA TECNICA_EXPERT.PDF"
+        ficha: "FICHA TECNICA_EXPERT.PDF"
       },
       {
         img: "traveller.jpg",
         nombre: "Traveller",
-        ficha: "assets/fichas/FICHA TECNICA_TRAVELLER.PDF"
+        ficha: "FICHA TECNICA_TRAVELLER.PDF"
       },
       {
         img: "partner_f.jpg",
         nombre: "Partner Furgon",
         manual: "partner_f.pdf",
-        ficha: "assets/fichas/FICHA TECNICA_PARTNER_F.PDF"
+        ficha: "FICHA TECNICA_PARTNER_F.PDF"
       },
       {
         img: "partner_p.jpg",
         nombre: "Partner Patagonica",
         manual: "partner_p.pdf",
-        ficha: "assets/fichas/FICHA TECNICA_PARTNER_P.PDF"
+        ficha: "FICHA TECNICA_PARTNER_P.PDF"
       }
     ]
   }
