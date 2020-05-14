349fa20846832b2b9d78261e72d7b2b58331556b
Leandro <oleito@gmail.com>
2020-05-14 13:03:12 -0300

    actualizacion de vendedores

--- js/vendedores-planes-140513.js
+++ js/vendedores-planes-140513.js
@@ -17,21 +17,6 @@ var appVendedores = new Vue({
         email: "alejandro.david@parisautos.com.ar",
       },
       {
-        img: "Aballerino.jpg",
-        nombre: "Alejandro Ballerino",
-        sector: "Ejecutivo de ventas",
-        cel: "2664860125",
-        email: "alejandro.ballerino@parisautos.com.ar",
-      },
-      
-      {
-        img: "Frodriguez.jpg",
-        nombre: "Federico Rodriguez",
-        sector: "Ejecutivo de ventas",
-        cel: "2664893686",
-        email: "federico.rodriguez@parisautos.com.ar",
-      },
-      {
         img: "Fsutil.jpg",
         nombre: "Felipe Sutil",
         sector: "Ejecutivo de ventas",
@@ -51,13 +36,6 @@ var appVendedores = new Vue({
         sector: "Ejecutivo de ventas",
         cel: "2665133483",
         email: "lino.corzo@parisautos.com.ar",
-      },
-      {
-        img: "Rmercado.jpg",
-        nombre: "Rafael Mercado",
-        sector: "Ejecutivo de ventas",
-        cel: "2664201027",
-        email: "rafael.mercado@parisautos.com.ar",
       }
     ],
   },
